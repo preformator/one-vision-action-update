84d7f56f4cb3d72e751d4c548dbce3dd107108f4
Ivan Dončević <idoncev@gmail.com>
2021-03-13 16:55:33 +0100

    Update server ip address

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
         if test $BUILDER_IMAGE; then
             echo "Checking if builder image is up to date with deps from the repo..."
 
-            PIPELINE_RESULT=$(curl -F 'data=@package.json' http://34.91.15.31:1000/check?name=$BUILDER_IMAGE)
+            PIPELINE_RESULT=$(curl -F 'data=@package.json' http://34.91.128.166:1000/check?name=$BUILDER_IMAGE)
 
             if test $PIPELINE_RESULT; then
                 echo "Please Update builder pipeline... !!!"
